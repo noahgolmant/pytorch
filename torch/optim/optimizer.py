c920988ec05fc38511ca75dae7f43b7d0c03bd27
Noah Golmant <noah.golmant@gmail.com>
2020-03-18 10:33:22 -0700

    add strict flag to optimizer to load state dict without all old param

--- torch/optim/optimizer.py
+++ torch/optim/optimizer.py
@@ -8,8 +8,10 @@ from itertools import chain
 
 class _RequiredParameter(object):
     """Singleton class representing a required parameter for an Optimizer."""
+
     def __repr__(self):
         return "<required parameter>"
+
 
 required = _RequiredParameter()
 
@@ -34,9 +36,10 @@ class Optimizer(object):
         self.defaults = defaults
 
         if isinstance(params, torch.Tensor):
-            raise TypeError("params argument given to the optimizer should be "
-                            "an iterable of Tensors or dicts, but got " +
-                            torch.typename(params))
+            raise TypeError(
+                "params argument given to the optimizer should be "
+                "an iterable of Tensors or dicts, but got " + torch.typename(params)
+            )
 
         self.state = defaultdict(dict)
         self.param_groups = []
@@ -45,30 +48,30 @@ class Optimizer(object):
         if len(param_groups) == 0:
             raise ValueError("optimizer got an empty parameter list")
         if not isinstance(param_groups[0], dict):
-            param_groups = [{'params': param_groups}]
+            param_groups = [{"params": param_groups}]
 
         for param_group in param_groups:
             self.add_param_group(param_group)
 
     def __getstate__(self):
         return {
-            'defaults': self.defaults,
-            'state': self.state,
-            'param_groups': self.param_groups,
+            "defaults": self.defaults,
+            "state": self.state,
+            "param_groups": self.param_groups,
         }
 
     def __setstate__(self, state):
         self.__dict__.update(state)
 
     def __repr__(self):
-        format_string = self.__class__.__name__ + ' ('
+        format_string = self.__class__.__name__ + " ("
         for i, group in enumerate(self.param_groups):
-            format_string += '\n'
-            format_string += 'Parameter Group {0}\n'.format(i)
+            format_string += "\n"
+            format_string += "Parameter Group {0}\n".format(i)
             for key in sorted(group.keys()):
-                if key != 'params':
-                    format_string += '    {0}: {1}\n'.format(key, group[key])
-        format_string += ')'
+                if key != "params":
+                    format_string += "    {0}: {1}\n".format(key, group[key])
+        format_string += ")"
         return format_string
 
     def state_dict(self):
@@ -82,44 +85,55 @@ class Optimizer(object):
         """
         # Save ids instead of Tensors
         def pack_group(group):
-            packed = {k: v for k, v in group.items() if k != 'params'}
-            packed['params'] = [id(p) for p in group['params']]
+            packed = {k: v for k, v in group.items() if k != "params"}
+            packed["params"] = [id(p) for p in group["params"]]
             return packed
+
         param_groups = [pack_group(g) for g in self.param_groups]
         # Remap state to use ids as keys
-        packed_state = {(id(k) if isinstance(k, torch.Tensor) else k): v
-                        for k, v in self.state.items()}
-        return {
-            'state': packed_state,
-            'param_groups': param_groups,
+        packed_state = {
+            (id(k) if isinstance(k, torch.Tensor) else k): v
+            for k, v in self.state.items()
         }
+        return {"state": packed_state, "param_groups": param_groups}
 
-    def load_state_dict(self, state_dict):
+    def load_state_dict(self, state_dict, strict=True):
         r"""Loads the optimizer state.
 
         Arguments:
             state_dict (dict): optimizer state. Should be an object returned
                 from a call to :meth:`state_dict`.
+            strict (bool, optional): whether to strictly enforce that the keys
+                in :attr:`state_dict` match the keys returned by this optimizers'
+                :meth:`~torch.optim.Optimizer.state_dict` function. Default: ``True``
         """
         # deepcopy, to be consistent with module API
         state_dict = deepcopy(state_dict)
         # Validate the state_dict
         groups = self.param_groups
-        saved_groups = state_dict['param_groups']
+        saved_groups = state_dict["param_groups"]
 
-        if len(groups) != len(saved_groups):
-            raise ValueError("loaded state dict has a different number of "
-                             "parameter groups")
-        param_lens = (len(g['params']) for g in groups)
-        saved_lens = (len(g['params']) for g in saved_groups)
-        if any(p_len != s_len for p_len, s_len in zip(param_lens, saved_lens)):
-            raise ValueError("loaded state dict contains a parameter group "
-                             "that doesn't match the size of optimizer's group")
+        if strict:
+            if len(groups) != len(saved_groups):
+                raise ValueError(
+                    "loaded state dict has a different number of " "parameter groups"
+                )
+            param_lens = (len(g["params"]) for g in groups)
+            saved_lens = (len(g["params"]) for g in saved_groups)
+            if any(p_len != s_len for p_len, s_len in zip(param_lens, saved_lens)):
+                raise ValueError(
+                    "loaded state dict contains a parameter group "
+                    "that doesn't match the size of optimizer's group"
+                )
 
         # Update the state
-        id_map = {old_id: p for old_id, p in
-                  zip(chain(*(g['params'] for g in saved_groups)),
-                      chain(*(g['params'] for g in groups)))}
+        id_map = {
+            old_id: p
+            for old_id, p in zip(
+                chain(*(g["params"] for g in saved_groups)),
+                chain(*(g["params"] for g in groups)),
+            )
+        }
 
         def cast(param, value):
             r"""Make a deep copy of value, casting all tensors to device of param."""
@@ -141,7 +155,7 @@ class Optimizer(object):
         # State that is not assigned to params is copied as is (needed for
         # backward compatibility).
         state = defaultdict(dict)
-        for k, v in state_dict['state'].items():
+        for k, v in state_dict["state"].items():
             if k in id_map:
                 param = id_map[k]
                 state[param] = cast(param, v)
@@ -150,16 +164,16 @@ class Optimizer(object):
 
         # Update parameter groups, setting their 'params' value
         def update_group(group, new_group):
-            new_group['params'] = group['params']
+            new_group["params"] = group["params"]
             return new_group
-        param_groups = [
-            update_group(g, ng) for g, ng in zip(groups, saved_groups)]
-        self.__setstate__({'state': state, 'param_groups': param_groups})
+
+        param_groups = [update_group(g, ng) for g, ng in zip(groups, saved_groups)]
+        self.__setstate__({"state": state, "param_groups": param_groups})
 
     def zero_grad(self):
         r"""Clears the gradients of all optimized :class:`torch.Tensor` s."""
         for group in self.param_groups:
-            for p in group['params']:
+            for p in group["params"]:
                 if p.grad is not None:
                     p.grad.detach_()
                     p.grad.zero_()
@@ -189,34 +203,40 @@ class Optimizer(object):
         """
         assert isinstance(param_group, dict), "param group must be a dict"
 
-        params = param_group['params']
+        params = param_group["params"]
         if isinstance(params, torch.Tensor):
-            param_group['params'] = [params]
+            param_group["params"] = [params]
         elif isinstance(params, set):
-            raise TypeError('optimizer parameters need to be organized in ordered collections, but '
-                            'the ordering of tensors in sets will change between runs. Please use a list instead.')
+            raise TypeError(
+                "optimizer parameters need to be organized in ordered collections, but "
+                "the ordering of tensors in sets will change between runs. Please use a list instead."
+            )
         else:
-            param_group['params'] = list(params)
+            param_group["params"] = list(params)
 
-        for param in param_group['params']:
+        for param in param_group["params"]:
             if not isinstance(param, torch.Tensor):
-                raise TypeError("optimizer can only optimize Tensors, "
-                                "but one of the params is " + torch.typename(param))
+                raise TypeError(
+                    "optimizer can only optimize Tensors, "
+                    "but one of the params is " + torch.typename(param)
+                )
             if not param.is_leaf:
                 raise ValueError("can't optimize a non-leaf Tensor")
 
         for name, default in self.defaults.items():
             if default is required and name not in param_group:
-                raise ValueError("parameter group didn't specify a value of required optimization parameter " +
-                                 name)
+                raise ValueError(
+                    "parameter group didn't specify a value of required optimization parameter "
+                    + name
+                )
             else:
                 param_group.setdefault(name, default)
 
         param_set = set()
         for group in self.param_groups:
-            param_set.update(set(group['params']))
+            param_set.update(set(group["params"]))
 
-        if not param_set.isdisjoint(set(param_group['params'])):
+        if not param_set.isdisjoint(set(param_group["params"])):
             raise ValueError("some parameters appear in more than one parameter group")
 
         self.param_groups.append(param_group)
